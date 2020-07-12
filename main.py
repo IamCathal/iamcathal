cf3dc3491a6d94a46366845108c17e18aebec6d8
IamCathal <cathalsocallaghan@gmail.com>
2020-07-12 22:10:33 +0100

    Slight format fix

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import json
 
 BASEURL = "https://api.github.com"
 
+
 def getPRs(repoList):
     PRList = []
 
@@ -18,30 +19,30 @@ def getPRs(repoList):
                     "url": PR["html_url"],
                     "title":PR["title"],
                     "repo": PR["base"]["repo"]["html_url"],
+                    "pullRequestLink":PR["_links"]["html"]["href"],
                     "createdAt": PR["created_at"],
                     "mergedAt": PR["merged_at"]
                 }
                 PRList.append(PRObj)
-
-    for PR in PRList:
-        print(PR)
-
     return PRList
+
 
 def writeReadMe(recentPRs):
     staticText = """
 ## Howya doing I'm Cathal, a passionate 2nd year CS&IT student in NUIG
 
-I'm by no means an expert but I love working with golang and like to use python and javascript. I'm currently an [MLH Fellowship](https://fellowship.mlh.io/) intern which is amazing. It's an internship focused entirely on helping students start their journey in contributing to the world of open source. Not only are we learning about the best practices and design choices but we are also contributing to real world projects that make an impact
+I'm by no means an expert but I love working with golang and like to use python and javascript. 
+
+I'm currently an [MLH Fellowship](https://fellowship.mlh.io/) intern which is amazing. It's an internship focused entirely on helping students start their journey in contributing to the world of open source. Not only are we learning about the best practices and design choices but we are also contributing to real world projects that make an impact
 
 ### Heres are some recent PRs I've worked on:
-##### This readme is auto generated, checkout the [the source code](https://github.com/iamcathal/iamcathal/blob/master/main.py)
-| Title        | Repo         | Status |
-| ------------- |:-------------:| -----:|
+| | |
+| ------------- |:-------------:|
 """
     for PR in recentPRs:
-        staticText += f"| {PR['title']} | [link]({PR['repo']}) | {'Merged 🎉' if PR['mergedAt'] else 'Open'} |\n"
+        staticText += f"| [{PR['title']}]({PR['pullRequestLink']})| {'Merged 🎉' if PR['mergedAt'] else 'Open'} |\n"
 
+    staticText += "##### This readme is auto generated, checkout the [the source code](https://github.com/iamcathal/iamcathal/blob/master/main.py)"
     f = open("README.md", "w")
     f.write(staticText)
     f.close()
@@ -52,7 +53,8 @@ I'm by no means an expert but I love working with golang and like to use python 
 def main():
     repoList = [
         "poychang/github-dark-theme",
-        "pallets/click"
+        "pallets/click",
+        "astaxie/beego"
         ]
     PRList = getPRs(repoList)
     writeReadMe(PRList)
